ec36240d021f0a9102fc55fec08bdbcb788c2216
Rafael <parzivalx50@gmail.com>
2026-09-08 11:57:35 -0600

    otra vez

--- FactorialFor.js
+++ FactorialFor.js
@@ -12,3 +12,15 @@ function factorialFor(n) {
 // Ejemplo de uso:
 console.log(factorialFor(5)); // Resultado: 120git
 console.log(factorialFor(23)); // Resultado: 120git
+
+function mostrarSerieFactorial(limite) {
+    let factorial = 1;
+
+    for (let i = 1; i <= limite; i++) {
+        factorial *= i;
+        console.log(`El factorial de ${i} (${i}!) es: ${factorial}`);
+    }
+}
+
+// Ejemplo: Mostrar la serie para los primeros 5 números
+mostrarSerieFactorial(5);

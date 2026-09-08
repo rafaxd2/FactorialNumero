9bf5d5af29a83b36ecb1a5b419c09407cfee17f4
Rafael <parzivalx50@gmail.com>
2026-09-08 12:38:35 -0600

    addd

--- FactorialFor.js
+++ FactorialFor.js
@@ -24,3 +24,31 @@ function mostrarSerieFactorial(limite) {
 
 // Ejemplo: Mostrar la serie para los primeros 5 números
 mostrarSerieFactorial(5);
+
+
+function factorialFor(n) {
+    if (n < 0) return "El factorial no está definido para números negativos";
+
+    let resultado = 1;
+    for (let i = 2; i <= n; i++) {
+        resultado *= i;
+    }
+
+    return resultado;
+}
+
+// Ejemplo de uso:
+console.log(factorialFor(5)); // Resultado: 120git
+console.log(factorialFor(23)); // Resultado: 120git
+
+function mostrarSerieFactorial(limite) {
+    let factorial = 1;
+
+    for (let i = 1; i <= limite; i++) {
+        factorial *= i;
+        console.log(`El factorial de ${i} (${i}!) es: ${factorial}`);
+    }
+}
+
+// Ejemplo: Mostrar la serie para los primeros 5 números
+mostrarSerieFactorial2(5);
